c8e3389bf41d52fdc40a32cd1236162e158a21ec
Isaac Mirabueno <caasi.mirabueno@gmail.com>
2023-08-22 14:01:01 +0800

    Request parameters only not the object params

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::ReservationsController < ApplicationController
   def reserve
-    payload = ReservationAdapter.call(params)
-    @reservation = ReservationManager::ReservationConstructor.new(payload: payload, code: payload.code, email: payload.email)
-    if @reservation.reserve!
-      render json: @reservation.payload
+    reservation_adapter = ReservationAdapter.call(request.request_parameters)
+    reservation = ReservationManager::ReservationConstructor.new(payload: reservation_adapter.payload, code: reservation_adapter.code, email: reservation_adapter.email)
+    if reservation.reserve!
+      render json: reservation.payload
     else
       raise ApiException::BadRequest
     end
